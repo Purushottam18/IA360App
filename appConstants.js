a341b26de60187f7864f2ba40fab161ecc3aaa01
purushottam <purushottamsamlety18@gmail.com>
2025-02-10 00:35:40 +0530

    minor fixes

--- appConstants.js
+++ appConstants.js
@@ -1,6 +1,6 @@
 let SERVER_URL;
 
-SERVER_URL = `http://192.168.1.5:5000`;  // when using permanent wifi connection
+SERVER_URL = `http://192.168.1.7:5000`;  // when using permanent wifi connection
 // SERVER_URL = `http://192.168.71.32:5000`; // if using mobile hotspot connection change the ip address to the one shown in the setting
 // SERVER_URL = `http://192.168.204.70:5000`; // when using mobile usb tethering
 //  SERVER_URL = `http://10.0.3.2:5000`;
